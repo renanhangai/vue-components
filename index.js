c7323933942dd0eeb7608e0e8cbfb60bad7e1f22
renanhangai <hangai.renan@gmail.com>
2018-01-29 16:35:41 -0200

    Updated options for component

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import Component, {InputMaskManager} from './component.vue';
 
-InputMaskManager.install = function( vue ) {
-	vue.component( 'vue-input-mask', Component );
+InputMaskManager.install = function( vue, options ) {
+	if ( typeof(options) === 'string' )
+		options = { name: options };
+	options = options || {};
+	vue.component( options.name || 'vue-input-mask', Component );
 };
 export default InputMaskManager;
