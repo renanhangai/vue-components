4a62cd5ec565efe7797e111f25e496010276b390
renanhangai <hangai.renan@gmail.com>
2018-04-03 19:56:59 -0300

    Added basic functionality

--- src/select.vue
+++ src/select.vue
@@ -1,14 +1,34 @@
 <template lang="pug">
-div.vue-select(role="combobox" :class="selectClass" tabindex="0")
+div.vue-select(role="combobox" :class="selectClass" ref="select" @blur="onBlur" @keydown="onKeyDown")
+	div.vue-select__value-container(:tabindex="dropdownActive ? -1 : 0" @click="onContainerClick")
+		div.vue-select__placeholder {{placeholder}} MASKDMkasd
 
+	div.vue-select__list-container(:style="{ 'z-index': zIndex }")
+		.vue-select__search-container
+			input.vue-select__search(ref="input" @blur="onBlur" v-model="searchText")
+		ul.vue-select__list
+			li.vue-select__item(v-for="item in 5" tabindex="0") OI
 </template>
 <style lang="scss">
 .vue-select {
 	position: relative;
+	
+	.vue-select__list-container {
+		display: none;
+		
+		position: absolute;
+		top: 100%;
+	}
+
+
+	// Styles when active
+	&.vue-select--dropdown-active {
+		.vue-select__list-container { display: block; }
+	}
 };
 </style>
 <script>
-import Fuse from 'fuse.js';
+// import Fuse from 'fuse.js';
 
 const Select = {
 	mode: () => 'foundation6',
@@ -16,9 +36,17 @@ const Select = {
 		value: true,
 		items: true,
 		mode:  String,
+		disabled: Boolean,
+		placeholder: String,
+		
+		// Style options
+		zIndex: Number,
 	},
 	data() {
 		return {
+			dropdownActive: false,
+
+			searchText: "",
 		};
 	},
 	computed: {
@@ -33,12 +61,32 @@ const Select = {
 				'vue-select--disabled': this.disabled,
 				'vue-select--dropdown-active': this.dropdownActive,
 				'vue-select--foundation6': ( this.selectMode === 'foundation6' ),
+				'vue-select--bootstrap4': ( this.selectMode === 'bootstrap4' ),
 			};
 		},
 	},
-	created() {
-	},
 	methods: {
+		refreshList() {
+		},
+		onBlur() {
+			this.$nextTick( () => {
+				if ( this.$refs.select.contains( document.activeElement ) )
+					return;
+				this.dropdownActive = false;
+			});
+		},
+		onContainerClick() {
+			this.toggleActive( null, true );
+		},
+		onKeyDown( evt ) {
+			// this.toggleActive( true, true );
+		},
+		toggleActive( status, needFocus ) {
+			this.dropdownActive = ( status == null ) ? !this.dropdownActive : !!status;
+			if ( this.dropdownActive && needFocus )
+				this.$nextTick( () => { this.$refs.input.focus(); } );
+		
+		}
 	},
 	watch: {
 	},
